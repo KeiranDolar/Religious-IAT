63f169c60c95f04f4c407a3b97ba3b9626ae63d2
Jennifer Leigh Johnson <estherbrownley33@icloud.com>
2025-09-03 21:09:19 -0500

    Update raceiat.js
    changed it back...

--- raceiat.js
+++ raceiat.js
@@ -1,217 +1,87 @@
-define(['questAPI'], function(Quest){
-	var API = new Quest();
-	
-/**
-	Settings
-	**/
-	API.addSettings('logger', 
-	{
-		url: '/implicit/PiQuest'
-	});
+define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.js'], function(APIConstructor, iatExtension){
+    let API = new APIConstructor();
+    let global = API.getGlobal();
 
-    API.addQuestionsSet('basicQ',{
-		autoSubmit:true,
-		decline: true, 
-		required: true,
-		help: '<%= pagesMeta.number < 3 %>'
-	});
-	
-	API.addQuestionsSet('basicSelect', 
-	{
-		inherit:'basicQ',
-		type:'text'
-	});
-
-	// ### Questions-pp list 3 likes
-	API.addQuestionsSet({
-		enter1 : [{
-		    inherit: 'basicSelect',
-			stem: 'Please indicate the name of an object you particularly <b><%=questionsData.feel%></b>',
-			required: true
-		}],
-		enter2 : [{
-		    inherit: 'basicSelect',
-			stem: 'Please indicate the name of another object you particularly <b><%=questionsData.feel%></b>',
-			required: true
-		}],
-		enter3 : [{
-		    inherit: 'basicSelect',
-			stem: 'Please indicate the name of a third object you particularly <b><%=questionsData.feel%></b>',
-			required: true
-		}],
-		enter12 : [{
-			inherit: 'basicSelect',
-			name:'like12',
-			stem: 'Please write the name of the same object one more time, to avoid any typos ',
-			required: true
-		}]
-	});
-
-	API.addQuestionsSet({
-		like1 : [{
-		    inherit: 'enter1',
-			name:'like1',
-			data: {feel:'like'}
-		}],
-		like12 : [{
-		    inherit: 'enter12',
-			name:'like12'
-		}],
-		like2 : [{
-		    inherit: 'enter2',
-			name:'like2',
-			data: {feel:'like'}
-		}],
-		like22 : [{
-		    inherit: 'enter12',
-			name:'like22'
-		}],
-		like3 : [{
-		    inherit: 'enter3',
-			name:'like3',
-			data: {feel:'like'}
-		}],
-		like32 : [{
-		    inherit: 'enter12',
-			name:'like32'
-		}]
-	});
-	// ### Questions-pp list 3 dislikes
-	API.addQuestionsSet({
-		dislike1 : [{
-		    inherit: 'enter1',
-			name:'dislike1',
-			data: {feel:'dislike'}
-		}],
-		dislike12 : [{
-		    inherit: 'enter12',
-			name:'dislike12'
-		}],
-		dislike2 : [{
-		    inherit: 'enter2',
-			name:'dislike2',
-			data: {feel:'dislike'}
-		}],
-		dislike22 : [{
-		    inherit: 'enter12',
-			name:'dislike22'
-		}],
-		dislike3 : [{
-		    inherit: 'enter3',
-			name:'dislike3',
-			data: {feel:'dislike'}
-		}],
-		dislike32 : [{
-		    inherit: 'enter12',
-			name:'dislike32'
-		}]
-	});
-
-	
-    /**
-	    Pages
-	**/
-	API.addPagesSet('basicPage',{
-		noSubmit:false, 
-		v1style: 2,
-		decline: false,
-		progressBar: 'Page <%= pagesMeta.number %> out of 6',
-        pageValidationText:'Typo: the item should be typed exactly the same in both times.'
-	});
-	
-	
-	// ### Sequence
-	API.addSequence(
-	[
-	    {
-	        mixer: 'random',//randomize likes and dislikes
-	        data:
-    	    [
-    	        { 
-    	            mixer:'wrapper',
-    	            data: 
-    	            [
-    	                {
-    	                    inherit:'basicPage', 
-    	                    questions: [
-    	                        {inherit:'like1'}, 
-    	                        {inherit:'like12'}
-    	                    ],
-                            pageValidation: function(){
-                                var questions = API.getCurrent().questions;
-                                return questions.like1.response==questions.like12.response;
-                            }
-                        },
-    	                {
-    	                    inherit:'basicPage', 
-    	                    questions: [
-    	                        {inherit:'like2'}, 
-    	                        {inherit:'like22'}
-    	                    ],
-                            pageValidation: function(){
-                                var questions = API.getCurrent().questions;
-                                return questions.like2.response==questions.like22.response;
-                            }
-                        },
-    	                {
-    	                    inherit:'basicPage', 
-    	                    questions: [
-    	                        {inherit:'like3'}, 
-    	                        {inherit:'like32'}
-    	                    ],
-                            pageValidation: function(){
-                                var questions = API.getCurrent().questions;
-                                return questions.like3.response==questions.like32.response;
-                            }
-                        }
-            	   ]
-    	        },
-    	        { 
-    	            mixer:'wrapper',
-    	            data: 
-    	            [
-    	                {
-    	                    inherit:'basicPage', 
-    	                    questions: [
-    	                        {inherit:'dislike1'}, 
-    	                        {inherit:'dislike12'}
-    	                    ],
-                            pageValidation: function(){
-                                var questions = API.getCurrent().questions;
-                                return questions.dislike1.response==questions.dislike12.response;
-                            }
-                        },
-    	                {
-    	                    inherit:'basicPage', 
-    	                    questions: [
-    	                        {inherit:'dislike2'}, 
-    	                        {inherit:'dislike22'}
-    	                    ],
-                            pageValidation: function(){
-                                var questions = API.getCurrent().questions;
-                                return questions.dislike2.response==questions.dislike22.response;
-                            }
-                        },
-    	                {
-    	                    inherit:'basicPage', 
-    	                    questions: [
-    	                        {inherit:'dislike3'}, 
-    	                        {inherit:'dislike32'}
-    	                    ],
-                            pageValidation: function(){
-                                var questions = API.getCurrent().questions;
-                                return questions.dislike3.response==questions.dislike32.response;
-                            }
-                        }
-            	   ]
-    	        }
-    	   ]
-	    }
-    ]);
-	/**
-	Return the script to piquest's god, or something of that sort.
-	**/
-	return API.script;
+    return iatExtension({
+        category1 : {
+            name : global.blackLabels, //Will appear in the data.
+            title : {
+                media : {word : global.blackLabels}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'bm1_nc.jpg'},
+                {image: 'bm2_nc.jpg'},
+                {image: 'bm3_nc.jpg'},
+                {image: 'bf1_nc.jpg'},
+                {image: 'bf2_nc.jpg'},                 
+                {image: 'bf3_nc.jpg'}     
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },    
+        category2 : {
+            name : global.whiteLabels, //Will appear in the data.
+            title : {
+                media : {word : global.whiteLabels}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'wm1_nc.jpg'},
+                {image: 'wm2_nc.jpg'},
+                {image: 'wm3_nc.jpg'},
+                {image: 'wf1_nc.jpg'},
+                {image: 'wf2_nc.jpg'},
+                {image: 'wf3_nc.jpg'}
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },
+        attribute1 : {
+            name : 'Bad words',
+            title : {
+                media : {word : 'Bad words'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: global.negWords[0]},
+                {word: global.negWords[1]},
+                {word: global.negWords[2]},
+                {word: global.negWords[3]},
+                {word: global.negWords[4]},
+                {word: global.negWords[5]},
+                {word: global.negWords[6]},
+                {word: global.negWords[7]}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+        attribute2 : {
+            name : 'Good words',
+            title : {
+                media : {word : 'Good words'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: global.posWords[0]},
+                {word: global.posWords[1]},
+                {word: global.posWords[2]},
+                {word: global.posWords[3]},
+                {word: global.posWords[4]},
+                {word: global.posWords[5]},
+                {word: global.posWords[6]},
+                {word: global.posWords[7]}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+        base_url : {//Where are your images at?
+            image : global.baseURL
+        },
+        isTouch : global.$isTouch
+    });
 });
-
-
